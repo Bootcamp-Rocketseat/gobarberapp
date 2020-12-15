5c9f163958a5660d368795a2a19cc6cd6c602cea
Maykon A. Oliveira <maykon.oliveira@neoway.com.br>
2020-12-14 21:02:01 -0300

    Add project

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  root: true,
+  extends: [
+    '@react-native-community',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error'
+  }
+};
